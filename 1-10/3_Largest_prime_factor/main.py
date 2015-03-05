08418b4a39ccb6ca71dfab589706d6a9600c30fa
sumi <omochibuster@yahoo.co.jp>
2015-03-05 21:57:06 +0900

    c -> python

---
+++ 1-10/3_Largest_prime_factor/main.py
@@ -0,0 +1,28 @@
+﻿# coding:utf-8
+# 13195 の素因数は 5, 7, 13, 29 である.
+# 600851475143 の素因数のうち最大のものを求めよ.
+
+from math import sqrt
+
+def devideN(number, factor):
+	number //= factor
+	while number % factor == 0:
+		number //= factor;
+	return number
+
+num = N = 600851475143
+maxFactor = 0
+
+if num % 2 == 0:
+	num = devideN(num, 2)
+
+factor = 3
+maxFactor = sqrt(num)
+
+while num > 1 and factor <= maxFactor:
+	if num % factor == 0:
+		num = devideN(num, factor)
+		maxFactor = sqrt(num)
+	factor += 2
+
+print(N, "の素因数の中で最大のものは", num, "です")
