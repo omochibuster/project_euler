695e2aef6800dcc40ca0b9f837af6afb82af02fd
sumi <omochibuster@yahoo.co.jp>
2015-03-05 22:14:10 +0900

    インポート時に実行されないようにした
    13行目にインポート時に実行されないような変更を追加

--- 1-10/3_Largest_prime_factor/main.py
+++ 1-10/3_Largest_prime_factor/main.py
@@ -10,19 +10,20 @@ def devideN(number, factor):
 		number //= factor;
 	return number
 
-num = N = 600851475143
-maxFactor = 0
+if __name__ == "__main__":
+	num = N = 600851475143
+	maxFactor = 0
 
-if num % 2 == 0:
-	num = devideN(num, 2)
+	if num % 2 == 0:
+		num = devideN(num, 2)
 
-factor = 3
-maxFactor = sqrt(num)
+	factor = 3
+	maxFactor = sqrt(num)
 
-while num > 1 and factor <= maxFactor:
-	if num % factor == 0:
-		num = devideN(num, factor)
-		maxFactor = sqrt(num)
-	factor += 2
+	while num > 1 and factor <= maxFactor:
+		if num % factor == 0:
+			num = devideN(num, factor)
+			maxFactor = sqrt(num)
+		factor += 2
 
-print(N, "の素因数の中で最大のものは", num, "です")
+	print(N, "の素因数の中で最大のものは", num, "です")
