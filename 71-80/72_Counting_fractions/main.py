fc0ccd28f87f6d4997010a6d90f6373bc93ebb34
sumi <omochibuster@yahoo.co.jp>
2015-03-09 23:39:40 +0900

    solve problem 72

---
+++ 71-80/72_Counting_fractions/main.py
@@ -0,0 +1,40 @@
+﻿#coding: utf-8
+
+# nとdを正の整数として, 分数 n/d を考えよう. n<d かつ HCF(n,d)=1 のとき,
+# 真既約分数と呼ぶ.d <= 8について真既約分数を大きさ順に並べると, 以下を得る:
+# 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+# この集合は21個の要素をもつことが分かる.
+# d <= 1,000,000について, 真既約分数の集合は何個の要素を持つか?
+
+import prime
+from functools import reduce
+from time import time
+
+def primeFactorization(number, plist):
+	list = [number]
+	for i in plist:
+		if number % i == 0:
+			list.append(i)
+			number //= i
+			while number % i == 0:
+				number //= i
+			if number == 1:
+				break
+	return list
+
+end = 1000000
+ans = 0
+p = prime.getPrime(2)
+plist = [next(p)]
+start = time()
+
+for i in range(2, end + 1):
+	if plist[-1] < i:
+		plist.append(next(p))
+	if i == plist[-1]:
+		ans += i - 1
+	else:
+		ans += int(reduce(lambda x, y: x * (1 - 1 / y), primeFactorization(i, plist)))
+print(ans)
+end = time()
+print(end - start)
---
+++ 71-80/72_Counting_fractions/pdfcode.py
@@ -0,0 +1,37 @@
+﻿from math import sqrt
+
+limit = 1000000
+sieveLimit = (int(sqrt(limit)) - 1) // 2
+maxIndex = (limit - 1) // 2
+cache = [0 for i in range(maxIndex + 1)]
+for n in range(1, sieveLimit):
+	if cache[n] == 0: # 2*n + 1 is prime
+		p = 2 * n + 1
+		for k in range(2 * n * (n + 1), maxIndex + 1, p):
+			if cache[k] == 0:cache[k] = p
+			
+multiplier = 1
+while multiplier <= limit:
+	multiplier *= 2
+multiplier //= 2
+fractionCount = multiplier - 1 # powers of 2
+multiplier //= 2
+stepIndex = ((limit // multiplier) + 1) // 2
+
+for n in range(1, maxIndex + 1):
+	if n == stepIndex:
+		multiplier //= 2
+		stepIndex = ((limit // multiplier) + 1) // 2
+	if cache[n] == 0:
+		cache[n] = 2 * n
+		fractionCount = fractionCount + multiplier * cache[n]
+	else:
+		p = cache[n]
+		cofactor = (2 * n + 1) // p
+		if cofactor % p == 0:
+			factor = p 
+		else:
+			factor = p - 1
+		cache[n] = factor * cache[cofactor // 2]
+		fractionCount = fractionCount + multiplier * cache[n]
+print(fractionCount)
