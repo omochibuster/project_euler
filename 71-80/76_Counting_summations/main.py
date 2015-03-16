1db5b665f583ef336406abf921538ee24fcfe182
sumi <omochibuster@yahoo.co.jp>
2015-03-16 14:29:13 +0900

    solve

--- 71-80/76_Counting_summations/main.py
+++ 71-80/76_Counting_summations/main.py
@@ -8,3 +8,18 @@
 # 2 + 1 + 1 + 1
 # 1 + 1 + 1 + 1 + 1
 # 2つ以上の正整数の和としての100の表し方は何通りか.
+
+memo = { }
+def p(n, m1 = 0):
+    m = n if m1 == 0 else min(n, m1)
+    
+    if n == 0:
+        return 1
+    elif (n, m) in memo:
+        return memo[(n,m)]
+    else:
+        s = sum(p(n - d, d) for d in range(1, m + 1))
+        memo[(n,m)] = s
+        return s
+        
+print(p(100) - 1)
